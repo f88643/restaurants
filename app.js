73aae4f3fcffb69edb27a82a22a26d11f29e48d9
f88643 <148025335+f88643@users.noreply.github.com>
2023-10-19 17:38:17 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const { engine } = require('express-handlebars')
+const restaurants = require('./public/jsons/restaurant.json').results
+
+const app = express()
+const port = 3000
+
+app.engine('.hbs', engine({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
+app.set('views', './views');
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.redirect('/restaurants')
+})
+
+
+app.get('/restaurants', (req, res) => {
+  const keyword = req.query.keyword?.trim()
+  console.log(keyword)
+  const matchedRestaurants = keyword ? restaurants.filter((rts) =>
+    Object.values(rts).some((property) => {
+      if (typeof property === 'string' && property) {
+        return property.toLowerCase().includes(keyword.toLowerCase())
+      }
+      return false
+    })
+  ) : restaurants
+  res.render('index', { restaurants: matchedRestaurants, keyword })
+})
+
+app.get('/restaurant/:id', (req, res) => {
+  const id = req.params.id
+  const restaurant = restaurants.find((rest) => rest.id.toString() === id)
+  res.render('details', { restaurant })
+})
+
+app.listen(port, () => {
+  console.log(`express server is running on http://localhost:${port}`)
+})
+
